15ab935c02745543e02d4312ef73e089c4dd9c96
Michael Mell <mike.mell@nthwave.net>
2012-04-26 19:36:57 -0700

    use jQuery

--- lib/at_linksafe/helper_extensions/message_helper.rb
+++ lib/at_linksafe/helper_extensions/message_helper.rb
@@ -1,33 +1,33 @@
 module AtLinksafe
   module HelperExtensions
     module MessageHelper
-    
+
       def close_button_html
         "X (Close) " # you can override this method in a ActionView::Base.class_eval do...end
       end
-      
+
       def messages(arr, html_class, opts = {} )
         return '' if arr.nil? or arr.empty?
         id = "#{html_class}_messages"
         s = "<div id='#{id}' class='#{html_class}'>"
         if opts[:fade_delay_secs]
           s << %Q{
-<script type='text/javascript'>
-function fade_#{id}() { Effect.Fade('#{id}', { duration: 0.8 }); }
+<script>
+function fade_#{id}() { jQuery('##{id}').fadeOut(2000); }
 </script>
-<a href='#' onclick='javascript:fade_#{id}();return true;'>#{close_button_html}</a>
+<a href='#' onclick='javascript:fade_#{id}();return false;'>#{close_button_html}</a>
 }
         end
 
         if 1 == arr.size
           s << arr[0]
         else
-          s << '<ul><li>' << arr.join('</li><li>') << '</li></ul>' 
+          s << '<ul><li>' << arr.join('</li><li>') << '</li></ul>'
         end
         s << '</div>'
         s.html_safe
       end
-      
+
       def errors
         arr = []
         arr += @errors_now unless @errors_now.nil?
@@ -36,7 +36,7 @@ function fade_#{id}() { Effect.Fade('#{id}', { duration: 0.8 }); }
         controller.messages_empty_errors
         messages(arr, 'error')
       end
-      
+
       def alerts
         arr = []
         arr += flash[:alerts] unless flash[:alerts].nil?
